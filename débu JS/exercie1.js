3f73e50f599b9fd6b6fbe90ac953f3096278ec05
Cailliez Marc <marccailliez@yahoo.fr>
2024-03-25 14:23:11 +0100

    EXO

---
+++ débu JS/exercie1.js
@@ -0,0 +1,8 @@
+/* exercie 13 / 14 integration java  */
+
+alert("Coucou !");
+console.log("Ah que Coucou !");
+document.write("<p>Mon 1er script</p>" + "<p>Coucou beuh !</p>");
+document.getElementById("essai").innerHTML = "<h1>Rien à ajouter</h1>";
+/* document.write("<br>"); */
+/* document.write("<p>Coucou beuh !</p>"); */
