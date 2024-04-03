d989ffb1d2339842e102a1fd76d970c0c10561b9
Cailliez Marc <marccailliez@yahoo.fr>
2024-04-03 16:11:01 +0200

    contact evaluation

---
+++ jarditou_css/assets/js/contact.js
@@ -0,0 +1,97 @@
+/* javascript pour jarditou pour  evaluation */ 
+
+function verif(){
+
+    const name = document.getElementById("nom");
+    const firstname = document.getElementById("prenom");
+    const sex = document.getElementById("Sexe*");
+    const jour = document.getElementById("date");
+    const cp = document.getElementById("postal");
+    /* const town = document.getElementById("ville"); */
+    const email = document.getElementById("email");
+    const roulette = document.getElementById("Sujet*");
+    const commentaire = document.getElementById("question");
+    const validation = document.getElementById("acceptation");
+
+
+/* effacer message erreur  */
+    clearErrorMessages();
+
+/* validation des entrées */
+if (name.value.length < 1) {
+    setErrorMessage(name, "* Veuillez entrer votre nom.");
+  }
+  if (firstname.value.length < 1) {
+    setErrorMessage(firstname, "* Veuillez entrer votre prénom.");
+  }
+ /*  if (sex.value.length < 1) {
+    setErrorMessage(sex, "* Veuillez séléctionner votre sexe.");
+  } */
+
+  /*  if (jour.value.length < 8 || !isNumeric(jour.value)) {
+    setErrorMessage(jour, "* Entrez date de naissance valide.");
+  } */
+  if (cp.value.length !== 5 || !isNumeric(cp.value)) {
+    setErrorMessage(cp, "* Entrez un code postal à 5 chiffres.");
+  }
+ 
+  if (commentaire.value.length < 1) {
+    setErrorMessage(commentaire, "* Veuillez taper votre question.");
+  }
+  if (email.value.indexOf("@" && ".") < 0) {
+    setErrorMessage(email, "* Veuillez entrer une adresse email valide.");
+  } /* chercher pourquoi cette ligne ne veut pas fonctionner */
+
+
+
+ /* Afficher un message de confirmation si aucune erreur n'a été trouvée */
+
+ if (document.querySelectorAll(".error").length === 0) {
+    
+    let messagealert = document.createElement("p");
+
+  messagealert.className = "alertcolor";
+
+  messagealert.textContent = "Merci ! Votre formulaire a bien été envoyé."; 
+
+  document.body.appendChild(messagealert);
+ 
+    alert( "Merci ! Votre formulaire a bien été envoyé.");
+    alert.className('alertcolor');
+  }
+}
+
+
+/* Effacer les entrées du formulaire */
+function efface() {
+    document.getElementById("contactForm").reset();
+    document.getElementById("message").innerText = "";
+    /* document.querySelector("fieldset").style.backgroundColor = ""; */
+    document.getElementById("bouton").style.display = "flex";
+    /* document.getElementById("legend").style.display = "block"; */
+    clearErrorMessages();
+  }
+
+/* Afficher un message d'erreur */
+
+function setErrorMessage(element, message) {
+    const error = document.createElement("span");
+    error.classList.add("error");
+    error.innerText = message;
+    element.parentNode.insertBefore(error, element.nextSibling);
+  }
+
+
+  /* Vérifier si que des chiffres */
+
+function isNumeric(str) {
+    return str.match(/^\d+$/) !== null;
+  }
+
+  function clearErrorMessages() {
+    const errors = document.getElementsByClassName("error");
+  
+    while (errors.length > 0) {
+      errors[0].remove();
+    }
+  }
